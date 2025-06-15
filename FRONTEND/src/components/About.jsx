547340dc91832d58455ea991164cd3f0e2eb0b1c
Ptl-aman <Ptlaman1922@email.com>
2025-06-15 18:11:36 +0530

    Final commit with frontend and backend properly added

---
+++ FRONTEND/src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Food is not just something we eat — it's an experience, a story, a celebration of flavors.
+              At the heart of every delicious dish lies love, care, and a passion for taste.
+               From the very first bite,
+               your senses awaken to something truly magical — a perfect balance of tasty,
+               rich flavors and nourishing ingredients that make you feel alive.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ FRONTEND/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Aman</div>
+          <div className="right">
+            <p>Ayodhya, U.P India</p>
+            <p>Open: 08:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Aman Patel</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Aman Food House.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
